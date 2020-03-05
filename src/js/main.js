42bf2233f1852692bb62469d525e8642dbdec6b9
Pierre Matterne <pierre.matterne@gmail.com>
2020-03-05 13:47:38 +0100

    Comments on the js file

--- src/js/main.js
+++ src/js/main.js
@@ -6,94 +6,129 @@
 var app = new Vue({
 	el: '#app',
 	data: {
-		message: 'You loaded this page on ' + new Date().toLocaleString(),
-		currentPage : "pageAlbums",
-		currentAlbum : "",
-		// isMaskActive : false, 
-		currentPhotoUrl : '',
-		currentTheme : 'themeLight',
+		// bool
 		modalThemeOpened: false,
 		displayInfoPanel: false,
-		currentPhotoInfos: {},
 		isThereError: false,
-		errorMsg : "There is no error, for now…",
+		// String
+		currentPage: "pageAlbums",
+		currentAlbum: "",
+		currentTheme: 'themeLight',
+		errorMsg: "There is no error, for now…",
+		// dictionary object
+		currentPhotoInfos: {},		
 	},
 
+	// Initialisation
 	created(){
 		this.albums = albums; 
 		this.photos = photos;
 	},
 
 	methods: {
-		setThemeRight: function(theme){
-			this.currentTheme = theme;
-			this.modalThemeOpened = false;
-		},
+		/**
+		 * Change the theme and close the modale
+		 *
+		 * @param {String} theme The name of the new theme to use
+		 */
+		 setThemeRight: function(theme){
+		 	this.currentTheme = theme;
+		 	this.modalThemeOpened = false;
+		 },
 
-		openAlbum : function(aName){
-			this.currentPage = "pagePhotos";
-			this.currentAlbum = aName;
-		},
+		/**
+		 * Open the Photos page and display the content of an album
+		 *
+		 * @param {String} aName The name of the album to display
+		 */
+		 openAlbum: function(aName){
+		 	this.currentPage = "pagePhotos";
+		 	this.currentAlbum = aName;
+		 },
 
-		getBackToAlbums : function(){
-			this.currentPage = "pageAlbums";
+		/**
+ 		* Gets back to the albums page.
+ 		*/
+ 		getBackToAlbums: function(){
+ 			this.currentPage = "pageAlbums";
 			// désactiver les images ouvertes
 		},
 
-		openPhoto : function(anId){
-			document.getElementById('mask').classList.add('active');
+		/**
+		 * Opens and display a photo.
+		 *
+		 * @param {integer}  anId  An identifier
+		 */
+		 openPhoto: function(anId){
+		 	document.getElementById('mask').classList.add('active');
 			// Remplaçable par un v-if ou v-show ? avec transition ?
 			// this.currentPhotoUrl = getUrlFromId(anId) ;
 			let	width = window.innerWidth || document.body.clientWidth;
 			let height = window.innerHeight || document.body.clientHeight;
 
-			document.getElementById('thePicture').style.maxWidth=width-30 + 'px';
-			document.getElementById('thePicture').style.maxHeight=height-200 + 'px';
+			document.getElementById('thePhoto').style.maxWidth=width-30 + 'px';
+			document.getElementById('thePhoto').style.maxHeight=height-200 + 'px';
 
-			this.currentPhotoInfos = getAllinfos(anId);
+			this.currentPhotoInfos = this.getAllinfos(anId);
 
 		},
 
-		unMask : function(){
-			document.getElementById('mask').classList.remove('active');
+		/**
+		 * Remove the mask and close the displayed photo
+		 */
+		 unMask: function(){
+		 	document.getElementById('mask').classList.remove('active');
 			// Remplacer la ligne ci-dessus par un v-show + animation.
 			this.displayInfoPanel = false;
 		},
 
-		openModalTheme : function(){
-			this.modalThemeOpened = true;
-		}
+		/**
+ 		* Opens the modal to choose a theme.
+ 		*/
+ 		openModalTheme: function(){
+ 			this.modalThemeOpened = true;
+ 		},
+
+ 		/**
+ 		 * Gets all the infos for a photo.
+ 		 *
+ 		 * @param {integer} id The identifier
+ 		 * @return {list} All the data about one photo.
+ 		 */
+ 		 getAllinfos: function(id){
+ 		 	for (let photo of app.photos){
+ 		 		if (photo.id === id){ return photo;}
+ 		 	}
+ 		 }
 
 
-	},
+ 		},
 
-	computed: {
-		nbrAlbums(){
-			return this.albums.length;
-		},
-		nbrPhotosInAlbum(){
-			let nbrPhotos = 0;
-			for (let photo of app.photos){
-				if (photo.inAlbum === this.currentAlbum){ 
-					nbrPhotos += 1;
-				}
-			}
-			return nbrPhotos;
-		}
+ 		computed: {
+ 		/**
+ 		 * Return the number of albums in total
+ 		 *
+ 		 * @return {integer} the number of albums
+ 		 */
+ 		 nbrAlbums(){
+ 		 	return this.albums.length;
+ 		 },
 
-	}
-}); 
+ 		/**
+ 		 * Return the number of photos in the current openend album
+ 		 *
+ 		 * @return {integer} the number of photo
+ 		 */
+ 		 nbrPhotosInAlbum(){
+ 		 	let nbrPhotos = 0;
+ 		 	for (let photo of app.photos){
+ 		 		if (photo.inAlbum === this.currentAlbum){ 
+ 		 			nbrPhotos += 1;
+ 		 		}
+ 		 	}
+ 		 	return nbrPhotos;
+ 		 }
 
+ 		}
+ 	}); 
 
-// Pourquoi sortir cette fonction ?
-function getUrlFromId(id){
-	for (let photo of app.photos){
-		if (photo.id === id){ return 'medias/' + photo.filename;}
-	}
-}
-
-function getAllinfos(id){
-	for (let photo of app.photos){
-		if (photo.id === id){ return photo;}
-	}
-}
