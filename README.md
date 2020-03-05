271c736a4daeb86eac417be328fea9c4467f1a1b
Pierre Matterne <pierre.matterne@gmail.com>
2020-03-05 19:12:28 +0100

    Added some small image + bug on the README.md

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
 # ramboutan
-Exercice de création : une interface de gestion d'images
+Exercice de création : une interface de gestion d'images.
 
-BUGs :
+Réalisé en deux jours, dans le cadre d'un entretien d'embauche.
 
-• Quand on affiche une image, puis qu'on revient sur la page d'accueil, un media n'est pas trouvé.
+
+
+
+
+Bugs restants :
+
+• Quand on affiche une image, puis qu'on revient sur la page d'accueil, la console indique qu'un media n'est pas trouvé.
 -> Le DOM tente-t-il d'afficher une img dont la source aurait disparue de l'app vue ?
 
-* Quand on revient sur la page d'accueil depuis la page "About", le cadre où s'affiche une image est visible un court instant.
+• Quand on revient sur la page d'accueil depuis la page "About", le cadre où s'affiche une image est visible un court instant.
 
+• Pour les toutes petites images, les boutons affichés en dessous peuvent avoir un défaut d'affichage lié à leur largeur. Le panneau d'infos n'est plus superposé à l'image.
--- src/js/data.js
+++ src/js/data.js
@@ -67,5 +67,116 @@ const photos = [
 	width: 1600, // in px
 	height: 778, // in px
 	isAlbumCover: true
+},
+{
+	id: 7,
+	filename : "5x5_01.gif",
+	inAlbum : "Many more",
+	name: "Keys",
+	weight : 4, 
+	date : 20040308,
+	width: 170, // in px
+	height: 170, // in px
+	isAlbumCover: true
+},
+{
+	id: 8,
+	filename : "5x5_02.gif",
+	inAlbum : "Many more",
+	name: "Eclipse",
+	weight : 5, 
+	date : 20040301,
+	width: 170, // in px
+	height: 170, // in px
+	isAlbumCover: false
+},
+{
+	id: 9,
+	filename : "5x5_03.gif",
+	inAlbum : "Many more",
+	name: "Phone",
+	weight : 4, 
+	date : 20040315,
+	width: 170, // in px
+	height: 170, // in px
+	isAlbumCover: false
+},
+{
+	id: 10,
+	filename : "5x5_04.gif",
+	inAlbum : "Many more",
+	name: "Woods",
+	weight : 5, 
+	date : 20040318,
+	width: 170, // in px
+	height: 170, // in px
+	isAlbumCover: false
+},
+{
+	id: 11,
+	filename : "5x5_05.gif",
+	inAlbum : "Many more",
+	name: "Computer",
+	weight : 5, 
+	date : 20040303,
+	width: 170, // in px
+	height: 170, // in px
+	isAlbumCover: false
+},
+{
+	id: 12,
+	filename : "5x5_06.gif",
+	inAlbum : "Many more",
+	name: "Gun",
+	weight : 8, 
+	date : 20040311,
+	width: 170, // in px
+	height: 170, // in px
+	isAlbumCover: false
+},
+{
+	id: 13,
+	filename : "5x5_07.gif",
+	inAlbum : "Many more",
+	name: "Calendar",
+	weight : 5, 
+	date : 20040307,
+	width: 170, // in px
+	height: 170, // in px
+	isAlbumCover: false
+},
+{
+	id: 14,
+	filename : "5x5_08.gif",
+	inAlbum : "Many more",
+	name: "Saloon Door",
+	weight : 6, 
+	date : 20040306,
+	width: 170, // in px
+	height: 170, // in px
+	isAlbumCover: false
+},
+{
+	id: 15,
+	filename : "5x5_09.gif",
+	inAlbum : "Many more",
+	name: "Cutter",
+	weight : 4, 
+	date : 20040310,
+	width: 170, // in px
+	height: 170, // in px
+	isAlbumCover: false
+},
+{
+	id: 16,
+	filename : "5x5_10.gif",
+	inAlbum : "Many more",
+	name: "Geometry",
+	weight : 4, 
+	date : 20040309,
+	width: 170, // in px
+	height: 170, // in px
+	isAlbumCover: false
 }
+
 ];
